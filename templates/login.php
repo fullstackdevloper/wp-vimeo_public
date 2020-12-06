4ba2870fd226631afc62a2020f0c830b9ed67198
Andrew Hlavats <andrew.hlavats@gmail.com>
2020-12-06 03:34:00 -0500

    Customize login and signup pages

--- templates/login.php
+++ templates/login.php
@@ -6,18 +6,20 @@
             <form class="wp_vimeo_login_form wp_vimeo_form" id="wp_vimeo_login_form" action="<?php echo admin_url( 'admin-post.php' ); ?>" method="post">
                 <input type="hidden" name="action" value="wp_vimeo_login">
                 <input type="hidden" name="redirect" value="<?php echo get_permalink(); ?>"/>
-                <div class="wp_vimewo_fieldwrap">
-                    <input type="text" wp_vimeo_validation="required" class="wp_vimeo_input" name="wp_vimeo_login[user_login]" placeholder="Username"/>
-                </div>
-                <div class="wp_vimewo_fieldwrap">
-                    <input type="password" wp_vimeo_validation="required" class="wp_vimeo_input" name="wp_vimeo_login[user_password]" placeholder="Password"/>
+                <div class="wp_vimeo_row">
+                    <div class="wp_vimeo_col_6 wp_vimewo_fieldwrap">
+                        <input type="text" wp_vimeo_validation="required" class="wp_vimeo_input" name="wp_vimeo_login[user_login]" placeholder="Username"/>
+                    </div>
+                    <div class="wp_vimeo_col_6 wp_vimewo_fieldwrap">
+                        <input type="password" wp_vimeo_validation="required" class="wp_vimeo_input" name="wp_vimeo_login[user_password]" placeholder="Password"/>
+                    </div>
                 </div>
                 <div class="wp_vimeo_row wp_vimeo_btns_wrap">
                     <button type="submit" class="wp_vimeo_btn wp_vimeo_btn_blue wp_vimeo_btn_large"><?php _e('Login', 'wp-vimeo'); ?></button>
                 </div>
 				<div class="wp_vimewo_fieldwrap">
                     <a class="reset_password" href="<?php echo home_url(); ?>/reset-password"><?php _e('Reset Password', 'wp-vimeo'); ?></a>
-					
+
 					<a class="signup" href="<?php echo home_url(); ?>/signup"><?php _e('Create an account', 'wp-vimeo'); ?></a>
                 </div>
             </form>

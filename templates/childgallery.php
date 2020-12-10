f99af73a0b9b162fe2ec7ccf1f965c3306c01c76
upworkmukesh@gmail.com <upworkmukesh@gmail.com>
2020-12-10 16:06:38 +0530

    update child gallery style
    update child gallery style

--- templates/childgallery.php
+++ templates/childgallery.php
@@ -1,6 +1,45 @@
 <div id="wp-vimeo_gallery">
-    
-
+      <div class='row'>
+    <div class='col-md-5'>
+      <div class="carousel slide media-carousel" id="media">
+        <div class="carousel-inner">
+		<?php 
+            $all_subscribers = get_users([ 'role__in' => [ 'author', 'subscriber' ] ] );
+			$i=1;
+            foreach($all_subscribers as $all_subscriber){
+                $user_link = get_avatar_url( $all_subscriber->data->ID,960);
+                $user_thumb_link = get_avatar_url( $all_subscriber->data->ID,96);
+				if($i==1){
+		?>
+			<div class="item  active">
+            <div class="row galler_slide">
+		
+		<?php
+			}
+        ?>
+			  <div class="col-md-4">
+                <a class="thumbnail" href="javascript:;"><img  width="150" height="150"  alt="" src="<?php echo $user_thumb_link;?>"></a>
+				<span><?php echo $all_subscriber->data->user_login; ?></span>
+              </div>          
+        <?php if($i % 9==0){ ?>
+			</div>
+          </div>
+		  <div class="item">
+            <div class="row galler_slide">
+		<?php } ?>             
+           <?php if(count($all_subscribers)==$i){ ?>
+				</div>
+			</div>
+		   <?php } ?>		   
+          
+       <?php  $i++; } 	?>
+        </div>
+        <a data-slide="prev" href="#media" class="left carousel-control">‹</a>
+        <a data-slide="next" href="#media" class="right carousel-control">›</a>
+      </div>                          
+    </div>
+  </div>
+<?php /* ?>
 <div class="demo-gallery">
             <ul id="lightgallery" class="list-unstyled row">
 			
@@ -23,12 +62,10 @@
              <?php  $i++; } 	?>
             </ul>
         </div>
+		
+		<?php */ ?>
 
 
 
-	<script type="text/javascript">
-        jQuery(document).ready(function(){
-            jQuery('#lightgallery').lightGallery();
-        });
-        </script>
+	
 </div>

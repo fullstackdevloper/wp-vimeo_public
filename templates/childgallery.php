9bffd186a634a02aed86466757213a44e0c2dc37
upworkmukesh@gmail.com <upworkmukesh@gmail.com>
2020-12-10 12:42:56 +0530

    limit added
    limit added

--- templates/childgallery.php
+++ templates/childgallery.php
@@ -6,10 +6,13 @@
 			
 			<?php 
             $all_subscribers = get_users([ 'role__in' => [ 'author', 'subscriber' ] ] );
-
+			$i=0;
             foreach($all_subscribers as $all_subscriber){
                 $user_link = get_avatar_url( $all_subscriber->data->ID,960);
                 $user_thumb_link = get_avatar_url( $all_subscriber->data->ID,96);
+				if($i>14){
+					break;
+				}
             ?>
                 <li class="col-xs-6 col-sm-4 col-md-3" data-responsive="<?php echo $user_link; ?>" data-src="<?php echo $user_link; ?>">
                     <a href="">
@@ -17,7 +20,7 @@
                     </a>
 					<span><?php echo $all_subscriber->data->user_login; ?></span>
                 </li>
-             <?php   } 	?>
+             <?php  $i++; } 	?>
             </ul>
         </div>
 

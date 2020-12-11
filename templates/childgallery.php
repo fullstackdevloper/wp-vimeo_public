402c09eb34ed49d3c171a086a997667fcfd8545b
upworkmukesh@gmail.com <upworkmukesh@gmail.com>
2020-12-11 13:06:20 +0530

    fix child gallery style
    fix child gallery style

--- templates/childgallery.php
+++ templates/childgallery.php
@@ -11,7 +11,7 @@
                             $user_link = get_avatar_url( $subscriber->data->ID,960);
                             $user_thumb_link = get_avatar_url( $subscriber->data->ID,96);
 							$child_first_name = get_user_meta($subscriber->data->ID, 'child_first_name', true);
-
+                            
 							if ($i == 1) {
                     ?>
                     <div class="item active">
@@ -25,7 +25,7 @@
                                 </a>
                                 <span><?php echo $child_first_name; ?></span>
                             </div>
-                    <?php if ($i % 9==0) { ?>
+                    <?php if ($i % 9==0 && count($subscribers) !=$i) { ?>
                         </div>
                     </div>
                     <div class="item">
@@ -45,30 +45,3 @@
         </div>
     </div>
 </div>
-
-
-<?php /* ?>
-<div class="demo-gallery">
-            <ul id="lightgallery" class="list-unstyled row">
-
-			<?php
-            $all_subscribers = get_users([ 'role__in' => [ 'author', 'subscriber' ] ] );
-			$i=0;
-            foreach($all_subscribers as $all_subscriber){
-                $user_link = get_avatar_url( $all_subscriber->data->ID,960);
-                $user_thumb_link = get_avatar_url( $all_subscriber->data->ID,96);
-				if($i>14){
-					break;
-				}
-            ?>
-                <li class="col-xs-6 col-sm-4 col-md-3" data-responsive="<?php echo $user_link; ?>" data-src="<?php echo $user_link; ?>">
-                    <a href="">
-                        <img class="img-responsive" width="150" height="150" attr="" src="<?php echo $user_thumb_link; ?>">
-                    </a>
-					<span><?php echo $all_subscriber->data->user_login; ?></span>
-                </li>
-             <?php  $i++; } 	?>
-            </ul>
-        </div>
-
-		<?php */ ?>

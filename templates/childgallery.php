2a09dacaa3c41d5daf257ef33e669fccc25209f1
Andrew Hlavats <andrew.hlavats@gmail.com>
2020-12-10 22:11:12 -0500

    small changes to childgallery

--- templates/childgallery.php
+++ templates/childgallery.php
@@ -1,49 +1,57 @@
-<div id="wp-vimeo_gallery">
-      <div class='row'>
-    <div class='col-md-5'>
-      <div class="carousel slide media-carousel" id="media">
-        <div class="carousel-inner">
-		<?php 
-            $all_subscribers = get_users([ 'role__in' => [ 'author', 'subscriber' ] ] );
-			$i=1;
-            foreach($all_subscribers as $all_subscriber){
-                $user_link = get_avatar_url( $all_subscriber->data->ID,960);
-                $user_thumb_link = get_avatar_url( $all_subscriber->data->ID,96);
-				if($i==1){
-		?>
-			<div class="item  active">
-            <div class="row galler_slide">
-		
-		<?php
-			}
-        ?>
-			  <div class="col-md-4">
-                <a class="thumbnail" href="javascript:;"><img  width="150" height="150"  alt="" src="<?php echo $user_thumb_link;?>"></a>
-				<span><?php echo $all_subscriber->data->user_login; ?></span>
-              </div>          
-        <?php if($i % 9==0){ ?>
-			</div>
-          </div>
-		  <div class="item">
-            <div class="row galler_slide">
-		<?php } ?>             
-           <?php if(count($all_subscribers)==$i){ ?>
-				</div>
-			</div>
-		   <?php } ?>		   
-          
-       <?php  $i++; } 	?>
+<div id="wp_vimeo_gallery">
+    <div class='row'>
+        <div class='col-md-12'>
+            <div class="carousel slide media-carousel" id="media">
+                <div class="carousel-inner">
+                    <?php
+                        $subscribers = get_users([ 'role__in' => [ 'author', 'subscriber' ] ] );
+                        $i = 1;
+
+                        foreach ($subscribers as $subscriber) {
+                            $user_link = get_avatar_url( $subscriber->data->ID,960);
+                            $user_thumb_link = get_avatar_url( $subscriber->data->ID,96);
+							$child_first_name = get_user_meta($subscriber->data->ID, 'child_first_name', true);
+
+							if ($i == 1) {
+                    ?>
+                    <div class="item active">
+                        <div class="row gallery_slide">
+                    <?php
+                        }
+                    ?>
+                            <div class="col-md-4">
+                                <a class="thumbnail" href="javascript:;">
+                                    <img width="150" height="150"  alt="" src="<?php echo $user_thumb_link;?>">
+                                </a>
+                                <span><?php echo $child_first_name; ?></span>
+                            </div>
+                    <?php if ($i % 9==0) { ?>
+                        </div>
+                    </div>
+                    <div class="item">
+                        <div class="row gallery_slide">
+                    <?php } ?>
+
+                    <?php if (count($subscribers) == $i) { ?>
+                        </div>
+                    </div>
+                    <?php } ?>
+
+                    <?php  $i++; } 	?>
+                </div>
+                <a data-slide="prev" href="#media" class="left carousel-control">‹</a>
+                <a data-slide="next" href="#media" class="right carousel-control">›</a>
+            </div>
         </div>
-        <a data-slide="prev" href="#media" class="left carousel-control">‹</a>
-        <a data-slide="next" href="#media" class="right carousel-control">›</a>
-      </div>                          
     </div>
-  </div>
+</div>
+
+
 <?php /* ?>
 <div class="demo-gallery">
             <ul id="lightgallery" class="list-unstyled row">
-			
-			<?php 
+
+			<?php
             $all_subscribers = get_users([ 'role__in' => [ 'author', 'subscriber' ] ] );
 			$i=0;
             foreach($all_subscribers as $all_subscriber){
@@ -62,10 +70,5 @@
              <?php  $i++; } 	?>
             </ul>
         </div>
-		
+
 		<?php */ ?>
-
-
-
-	
-</div>

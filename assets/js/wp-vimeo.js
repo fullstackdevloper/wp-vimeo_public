ea04d073f73a165a92c0d9deb6bbfd8eb2e98444
upworkmukesh@gmail.com <upworkmukesh@gmail.com>
2020-12-11 19:32:46 +0530

    updated
    updated

--- assets/js/wp-vimeo.js
+++ assets/js/wp-vimeo.js
@@ -129,8 +129,8 @@ var wpVimeo;
         {
             breakpoint: 1024,
             settings: {
-                slidesToShow: 3,
-                slidesToScroll: 3,
+                slidesToShow: 1,
+                slidesToScroll: 1,
             }
         },
         {
